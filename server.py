5e680ab1cde6eb9871e059b20fcc85ddd595d20d
Prim5v <emax27348@gmail.com>
2025-09-10 12:02:46 +0300

    Update server.py

--- server.py
+++ server.py
@@ -36,7 +36,8 @@ cloudinary.config(
 load_dotenv()
 app = Flask(__name__)
 CORS(app)  # Enable CORS
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
+
 
 app.config['UPLOAD_FOLDER'] = '/home/studyhub4293/mysite/static/uploads'
 
